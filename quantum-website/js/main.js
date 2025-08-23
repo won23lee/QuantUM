9129832511c500fda169ad01fff90972cb008e2b
Won Lee <133627199+won23lee@users.noreply.github.com>
2025-08-23 11:52:35 -0400

    Delete quantum-website directory

--- quantum-website/js/main.js
+++
@@ -1,9 +0,0 @@
-
-// Simple active link highlighter
-(function(){
-  const here = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.menu a').forEach(a=>{
-    const href = a.getAttribute('href');
-    if(href === here){ a.classList.add('active'); }
-  });
-})();
